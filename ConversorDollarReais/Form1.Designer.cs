dac004470168ade1f2192fe04a08f3338bb530d5
Douglas-ao <douglas.o@edu.unipar.br>
2021-10-01 19:02:07 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorDollarReais/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace ConversorDollarReais
+{
+    partial class CalcularDolar
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblRe = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(80, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Qual é o preço do Dólar:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(248, 12);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(163, 22);
+            this.txtQuantidade.TabIndex = 1;
+            this.txtQuantidade.TextChanged += new System.EventHandler(this.txtQuantidade_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Quantos dólar serão comprados:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(248, 60);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(163, 22);
+            this.txtValor.TabIndex = 3;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = " Valor a ser pago em Reais:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(418, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(90, 70);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Calcular";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(269, 109);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 7;
+            // 
+            // lblRe
+            // 
+            this.lblRe.AutoSize = true;
+            this.lblRe.BackColor = System.Drawing.Color.White;
+            this.lblRe.Location = new System.Drawing.Point(248, 109);
+            this.lblRe.Name = "lblRe";
+            this.lblRe.Size = new System.Drawing.Size(0, 17);
+            this.lblRe.TabIndex = 9;
+            // 
+            // CalcularDolar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(533, 154);
+            this.Controls.Add(this.lblRe);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.label1);
+            this.Name = "CalcularDolar";
+            this.Text = "Calcular";
+            this.Load += new System.EventHandler(this.CalcularDolar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblRe;
+    }
+}
+
---
+++ ConversorDollarReais/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorDollarReais
+{
+    public partial class CalcularDolar : Form
+    {
+        private int priceDollar;
+        private int dollars;
+        private int percent;
+
+        public CalcularDolar()
+        {
+            InitializeComponent();
+        }
+
+        private void txtQuantidade_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            double qtdDolar;
+            double precoDolar;
+            double porcento = 3;
+            double total;
+            qtdDolar = Convert.ToInt32(txtQuantidade.Text);
+            precoDolar = Convert.ToDouble(txtValor.Text);
+            porcento = (precoDolar * qtdDolar) * porcento / 100;
+            total = (precoDolar * qtdDolar) + porcento;
+            lblRe.Text = Convert.ToString(total.ToString("C"));
+
+            
+        }
+
+        private void CalcularDolar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
